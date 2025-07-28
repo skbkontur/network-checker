1f0bfa5b878cfa0ce03788d2e30128a7f0f33617
Evgeniy Zhuravlev <juravlev.johm@gmail.com>
2025-07-28 14:42:03 +0500

    fix geoip local folder resolve

--- tracer.go
+++ tracer.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"kontur.ru/edoops/network-checker/ztrace"
+	"fmt"
+	"os"
 	"time"
+
+	"kontur.ru/edoops/network-checker/ztrace"
 )
 
+func resolveGeoipDir() string {
+	localDir := "geoip"
+	if _, err := os.Stat(localDir); err != nil {
+		return "/usr/local/lib/geoip"
+	}
+
+	return localDir
+}
+
 func traceroute(destination string, ports []uint16) {
-	t := ztrace.New("icmp", destination, "", 30, uint8(64), float32(3), 0, false, "geoip/asn.mmdb", "geoip/geoip.mmdb")
+	geoipDir := resolveGeoipDir()
+	t := ztrace.New(
+		"icmp", destination, "", 30, uint8(64), float32(3), 0, false,
+		fmt.Sprintf("%s/asn.mmdb", geoipDir), fmt.Sprintf("%s/geoip.mmdb", geoipDir))
 	t.Latitude = 31.02
 	t.Longitude = 121.1
 	t.TCPProbePorts = ports
